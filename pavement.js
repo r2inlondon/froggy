2c891f6508454a9baa7a2688784f67d7d4fbdb28
Arturo Quiroz <r2inlondon@gmail.com>
2021-08-21 10:43:56 +0100

    central line added but needs some Math

--- pavement.js
+++ pavement.js
@@ -5,13 +5,35 @@
 const myCanvas = document.getElementById('myCanvas'),
       ctx = myCanvas.getContext('2d');
 
+let x = 20, y = 20, carWidthRight = 30, carHeightRight = 15, carWidthLeft = 30, carHeightLeft = 15, streetHeight = 40;
+
+function drawCarImageLeft(){        
+    carImageLeft = new Image();
+    carImageLeft.src = 'img/purple-car.svg';
+    
+    ctx.drawImage(carImageLeft, x, y, carWidthLeft, carHeightLeft);
+            
+}
 
 function street(){
+    let streetBorder = streetHeight;
+    let centreLine = 5;
+    
     ctx.beginPath();
-    ctx.rect(-5, 20, 310, 35);
+    ctx.rect(-5, 20, 310, streetHeight);
     ctx.fillStyle = "#CAC4BD";
     ctx.fill();
     ctx.stroke();
+
+    for(let i = 0; i < 9; i++){
+        ctx.beginPath();
+        ctx.strokeStyle = "gray";
+        ctx.moveTo(centreLine, streetBorder);
+        ctx.lineTo(centreLine += 30, streetBorder);
+        ctx.closePath();
+        ctx.stroke();
+        centreLine += 30
+    }
 }
 
 
@@ -19,6 +41,8 @@ function street(){
 function animate(){
             
     street();
+
+    drawCarImageLeft();
     
     requestAnimationFrame(animate);    
     
