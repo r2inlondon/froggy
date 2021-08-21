5c3e567a06ccdc4044b7a357be0d1a288982c918
Arturo Quiroz <r2inlondon@gmail.com>
2021-08-21 10:29:16 +0100

    pavement colour and limits

--- pavement.js
+++ pavement.js
@@ -4,3 +4,28 @@
 // Selectors
 const myCanvas = document.getElementById('myCanvas'),
       ctx = myCanvas.getContext('2d');
+
+
+function street(){
+    ctx.beginPath();
+    ctx.rect(-5, 20, 310, 35);
+    ctx.fillStyle = "#CAC4BD";
+    ctx.fill();
+    ctx.stroke();
+}
+
+
+      
+function animate(){
+            
+    street();
+    
+    requestAnimationFrame(animate);    
+    
+}
+
+function startGame(){
+    
+    animate();
+        
+}
