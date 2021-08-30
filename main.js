848be65f5722d2d194a4fb77cc9a869c80cc70d8
Arturo Quiroz <r2inlondon@gmail.com>
2021-08-30 16:24:05 +0100

    all lanes are with cars now

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { drawMotorway } from "./modules/motorway.js";
 
 
 // animation function
-function animate(leftCar1, carsRight1, leftCar2){
+function animate(leftCar1, carsRight1, leftCar2, carsRight2, leftCar3, carsRight3){
     // clear frog and cars previous position
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
     // Draw motherWay
@@ -13,11 +13,16 @@ function animate(leftCar1, carsRight1, leftCar2){
 
     leftCar1.forEach( car => car.drawLeft());
     carsRight1.forEach( car => car.drawRight());
+
     leftCar2.forEach( car => car.drawLeft());
+    carsRight2.forEach( car => car.drawRight());
+
+    leftCar3.forEach( car => car.drawLeft());
+    carsRight3.forEach( car => car.drawRight());
 
     drawFrogImage(xFrog, yFrog);
 
-    requestAnimationFrame(() => animate(leftCar1, carsRight1, leftCar2));          
+    requestAnimationFrame(() => animate(leftCar1, carsRight1, leftCar2, carsRight2, leftCar3, carsRight3));          
 }
 
 
@@ -33,14 +38,17 @@ start.addEventListener('click', () => {
     window.addEventListener('keydown', moveFrog);
     window.addEventListener('keyup', releasedKey); 
 
-    const leftCar1 = laneDirectionLeft(2, 14, 0.5);  
-    const carsRight1 =  laneDirectionRight(3,34,0.5);
-    const leftCar2 = laneDirectionLeft(1, 54, 3);  
+    const   leftCar1 = laneDirectionLeft(3, 14, 0.5),  
+            carsRight1 =  laneDirectionRight(2,34,1),
+            leftCar2 = laneDirectionLeft(1, 54, 4),
+            carsRight2 =  laneDirectionRight(2,74,0.5),
+            leftCar3 = laneDirectionLeft(3, 94, 1),
+            carsRight3 =  laneDirectionRight(4,114,1);
     
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
     
     if(gameOn === false){
-        animate(leftCar1, carsRight1, leftCar2);
+        animate(leftCar1, carsRight1, leftCar2, carsRight2, leftCar3, carsRight3);
         gameOn = true;
     }            
 
